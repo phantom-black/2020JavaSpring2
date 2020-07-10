8d792455fb70a1f0e96eda12d1dd2895a76604bb
phantom-black <black_phantom@naver.com>
2020-07-10 12:44:21 +0900

    자바 스프링 백엔드 수업(2020.07.10.)_추가

--- Second/src/com/koreait/second/dao/OracleDAO.java
+++ Second/src/com/koreait/second/dao/OracleDAO.java
@@ -5,6 +5,18 @@ import java.sql.DriverManager;
 
 public class OracleDAO {
 	
+	private static OracleDAO dao = null;
+	
+	private OracleDAO() {}
+	
+	public static OracleDAO getInstance() { // 싱글톤 : getInstance()로 이름 맞추는 듯, 값이 null일 경우에 새 객체 생성, 이후 계속 같은 칭구 리턴
+		if(dao == null) {
+			dao = new OracleDAO();
+		}
+		return dao;
+	}
+	
+	/*
 	public static void main(String[] args) {
 		OracleDAO dao = new OracleDAO();
 		try {
@@ -14,6 +26,7 @@ public class OracleDAO {
 		}
 		
 	}
+	*/
 	
 	public Connection getConn() throws Exception { // try catch문 대신 throws Exception로 예외 넘기기도 가능 
 		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
@@ -25,5 +38,13 @@ public class OracleDAO {
 		System.out.println("접속 성공!!!");
 		
 		return con;
+	}
+	
+	public void closeConn(Connection conn) {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch(Exception e) {}
+		}
 	}
 }
